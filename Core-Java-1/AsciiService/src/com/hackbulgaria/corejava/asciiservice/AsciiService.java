67850f57d993dd465f1746ddfbb27322b6383483
rgeorgiev583 <rgeorgiev583@gmail.com>
2014-07-29 20:34:24 +0300

    added AsciiService web app

---
+++ Core-Java-1/AsciiService/src/com/hackbulgaria/corejava/asciiservice/AsciiService.java
@@ -0,0 +1,39 @@
+package com.hackbulgaria.corejava.asciiservice;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AsciiService
+ */
+@WebServlet("/AsciiService")
+public class AsciiService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AsciiService() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
